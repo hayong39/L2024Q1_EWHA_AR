39b6b393c4cebc7325d36751212652bc4b810e60
황주선 <lectures.202x@gmail.com>
2024-04-12 11:11:39 +0900

    W06 GSwAR

---
+++ Assets/W06_GSwAR_Sample/W06_GSwAR_ClickObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W06_GSwAR_ClickObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        print(gameObject.name + " clicked.");
+    }
+}
